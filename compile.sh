a65faa61078e26d0d8ef0b91618486d5997c0f99
Frederic Kehrein <frederic@kehrein.org>
2017-11-07 00:45:51 +0100

    compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,4 @@
-fpc  -MObjFPC -Scghi -CX -O3 -XX -l -vewnhibq -Filib -FuLazTextForms -Fu. -FUlib -FEbin/Release/
+mkdir bin
+mkdir bin/Release
+mkdir lib
+fpc  -MObjFPC -Scghi -FiLazTextForms -CX -O3 -XX -l -vewnhibq -Filib -FuLazTextForms -Fu. -FUlib -FEbin/Release/ ./PokeTeamBuilder.lpr
